1e1567fcfdbf86fa58af88f31f0dd8341157424a
Tung <admiralkurayami@gmail.com>
2022-04-24 17:19:18 +0700

    xong

---
+++ basicjs.js
@@ -0,0 +1,5 @@
+let inputYear = prompt('nhap vao nam');
+let inputMonth = prompt('nhap vao thang');
+let inputDay = prompt('nhap vao ngay');
+
+document.write("hom nay la:" + inputDay + "-" + inputMonth + "-" + inputYear)
